30545cb53eb9b3df1142e96b73a1c0b5fa4db95c
Михаел Йорданов <gaws@mail.bg>
2024-07-12 07:13:32 +0300

    use custom components

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,15 @@
+import { HelloWave } from "@/components/HelloWave";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
 import { Image, StyleSheet, Text, View } from "react-native";
 const logo = require("../assets/images/react-logo.png");
 export default function Index() {
   return (
-    <View style={styles.container}>
+    <ThemedView style={styles.container}>
       <Image source={logo} style={styles.logo} />
-      <Text>Wellcome page</Text>
-    </View>
+      <ThemedText type="title">Wellcome page</ThemedText>
+      <HelloWave />
+    </ThemedView>
   );
 }
 const styles = StyleSheet.create({
@@ -13,6 +17,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    gap: 16,
   },
   logo: {
     width: 100,
